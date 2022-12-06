58a55eebf464dbbaa3a0fbd4cf7cbeaf472395e1
keemeesuu <kimhogy@gmail.com>
2022-12-06 23:29:14 +0900

    miso update

--- samples/miso/lib/main.dart
+++ samples/miso/lib/main.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'miso.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: AppListPage(),
+    );
+  }
+}
+
+class AppListPage extends StatelessWidget {
+  const AppListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            // Miso
+            ListTile(
+              title: Text("1. Miso", style: TextStyle(fontSize: 24)),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => Miso()),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
